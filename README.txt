62f4c05c01fec9483e800c77d651ef4edd1fdb65
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-12-29 12:16:00 +0000

    init import (skeleton only)

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This package extends the default behavior for simplelayout's file-block. 
+It changes the default block_view on files with a flowplayer enabled block_view
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = open('simplelayout/types/flowplayerblock/version.txt').read().strip()
+
+setup(name='simplelayout.types.flowplayerblock',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Mathias LEIMGRUBER (4teamwork)',
+      author_email='m.leimgruber@4teamwork.ch',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['simplelayout', 'simplelayout.flowplayerblock'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.flowplayer'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
