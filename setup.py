e7a902d56fd29b5874fc5211d0af2a4fcaf3e555
Jonas Baumann <jone@jone.ch>
2012-10-26 15:54:47 +0200

    Rename readme to .rst.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a3.dev0'
 setup(name='simplelayout.types.flowplayerblock',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" + \
+      long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
 
       # Get more strings from
