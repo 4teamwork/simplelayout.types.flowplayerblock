391c0c47c8cf7aeacff686cad50f0ef155bd2869
Jonas Baumann <jone@jone.ch>
2012-10-26 15:54:00 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -6,31 +6,35 @@ version = '1.0a3.dev0'
 setup(name='simplelayout.types.flowplayerblock',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
+
       keywords='',
-      author='Mathias LEIMGRUBER (4teamwork)',
-      author_email='m.leimgruber@4teamwork.ch',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/simplelayout.types.flowplayerblock',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['simplelayout', 'simplelayout.types'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'collective.flowplayer',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'collective.flowplayer',
+        # -*- Extra requirements: -*-
+        ],
+
       entry_points="""
       # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
